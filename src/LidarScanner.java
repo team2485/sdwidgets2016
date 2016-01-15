324997b65c263c7999111a293464fb619197d71a
amanda-wasserman <awasserman2019@francisparker.org>
2016-01-14 18:34:15 -0800

    Lidar Scanner Created!!!
    Created variables in Lidar Scanner class

---
+++ src/LidarScanner.java
@@ -0,0 +1,29 @@
+import edu.wpi.first.smartdashboard.gui.Widget;
+import edu.wpi.first.smartdashboard.properties.Property;
+import edu.wpi.first.smartdashboard.types.DataType;
+
+public class LidarScanner extends Widget {
+
+	public static final String NAME = "2485 Scanner";
+	public static final DataType[] TYPES = {DataType.STRING};
+	
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}	
+	
+	@Override
+	public void propertyChanged(Property arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setValue(Object arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
