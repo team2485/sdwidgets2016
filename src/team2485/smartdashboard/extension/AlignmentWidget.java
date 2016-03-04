de93338159446ea8a25025d1d064c259c768a1d3
Nicholas Contreras <ncontreras2018@francisparker.org>
2016-03-03 22:39:45 -0800

    Added non-GRIP based Axis camera alignment widget

---
+++ src/team2485/smartdashboard/extension/AlignmentWidget.java
@@ -0,0 +1,28 @@
+package team2485.smartdashboard.extension;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import edu.wpi.first.smartdashboard.gui.elements.VideoStreamViewerExtension;
+import edu.wpi.first.smartdashboard.properties.NumberProperty;
+
+public class AlignmentWidget extends VideoStreamViewerExtension {
+
+	NumberProperty linePos = new NumberProperty(this, "Line Location", 0.75);
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		Graphics2D g2d = (Graphics2D) g;
+
+		g2d.setColor(Color.RED);
+		g2d.setStroke(new BasicStroke(5));
+
+		g2d.drawLine((int) ((double) linePos.getValue() * getWidth()), 0,
+				(int) ((double) linePos.getValue() * getWidth()), getHeight());
+
+	}
+}
