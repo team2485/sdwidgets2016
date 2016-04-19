7ae063b2ace26c413a2d276e183a2aae1e084137
Aidan Fay <afay2017@francisparker.org>
2016-04-19 13:08:53 -0700

    More features to Trim Widget, Alignment widget with NetworkTables

--- src/team2485/smartdashboard/extension/AlignmentWidget.java
+++ src/team2485/smartdashboard/extension/AlignmentWidget.java
@@ -6,25 +6,58 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 
 import edu.wpi.first.smartdashboard.gui.elements.VideoStreamViewerExtension;
+import edu.wpi.first.smartdashboard.properties.BooleanProperty;
 import edu.wpi.first.smartdashboard.properties.NumberProperty;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import edu.wpi.first.wpilibj.tables.TableKeyNotDefinedException;
 
 public class AlignmentWidget extends VideoStreamViewerExtension {
-	
+
 	public static final String NAME = "Logicless Alignment Widget";
 
-	NumberProperty linePos = new NumberProperty(this, "Line Location", 0.75);
+	public final NumberProperty linePosBatter = new NumberProperty(this,
+			"Batter Shot Line", 240);
+	public final NumberProperty linePosLong = new NumberProperty(this,
+			"Long Shot Line", 240);
+
+	public final BooleanProperty pullFromNetwork = new BooleanProperty(this,
+			"Match Lines From Trim", true);
 
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 
+		if (pullFromNetwork.getValue()) {
+
+			try {
+
+				linePosBatter.setValue(NetworkTable.getTable("SmartDashboard")
+						.getNumber("Batter Shot Alignment"));
+				linePosLong.setValue(NetworkTable.getTable("SmartDashboard")
+						.getNumber("Long Shot Alignment"));
+			} catch (TableKeyNotDefinedException e) {
+			}
+		}
+
+		double widthRatio = getWidth() / 320.0;
+
+		double heightRatio = getHeight() / 240.0;
+
 		Graphics2D g2d = (Graphics2D) g;
 
-		g2d.setColor(Color.RED);
 		g2d.setStroke(new BasicStroke(5));
 
-		g2d.drawLine((int) ((double) linePos.getValue() * getWidth()), 0,
-				(int) ((double) linePos.getValue() * getWidth()), getHeight());
+		g2d.setColor(Color.RED);
+
+		g2d.drawLine((int) (linePosBatter.getValue().intValue() * widthRatio),
+				0, (int) (linePosBatter.getValue().intValue() * widthRatio),
+				(int) (100 * heightRatio));
+
+		g2d.setColor(Color.BLUE);
+
+		g2d.drawLine((int) (linePosLong.getValue().intValue() * widthRatio),
+				(int) (100 * heightRatio), (int) (linePosLong.getValue()
+						.intValue() * widthRatio), getHeight());
 
 	}
 }
--- src/team2485/smartdashboard/extension/TrimWidget.java
+++ src/team2485/smartdashboard/extension/TrimWidget.java
@@ -9,17 +9,22 @@ import java.awt.event.KeyListener;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 import edu.wpi.first.smartdashboard.gui.Widget;
+import edu.wpi.first.smartdashboard.properties.BooleanProperty;
+import edu.wpi.first.smartdashboard.properties.IntegerProperty;
+import edu.wpi.first.smartdashboard.properties.NumberProperty;
 import edu.wpi.first.smartdashboard.properties.Property;
 import edu.wpi.first.smartdashboard.properties.StringProperty;
 import edu.wpi.first.smartdashboard.types.DataType;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 public class TrimWidget extends Widget implements ActionListener, KeyListener {
-	
+
 	public static final String NAME = "Value Trimmer";
 
 	public static final DataType[] TYPES = { DataType.NUMBER };
@@ -27,19 +32,47 @@ public class TrimWidget extends Widget implements ActionListener, KeyListener {
 	public final StringProperty dataName = new StringProperty(this,
 			"Value Name", "Value");
 
-	private JButton trimLeft;
-	private JTextField trimValue;
-	private JButton trimRight;
+	public final StringProperty leftButtonText = new StringProperty(this,
+			"Left Button Text", "<--");
+	public final StringProperty rightButtonText = new StringProperty(this,
+			"Right Button Text", "-->");
 
-	private int value;
+	public final BooleanProperty inverted = new BooleanProperty(this,
+			"Inverted", false);
+
+	public final NumberProperty unitsPerClick = new NumberProperty(this,
+			"Units Per Click", 1);
+
+	private JButton leftButton;
+	private JTextField trimValue;
+	private JButton rightButton;
+	private JLabel label;
+
+	private double value;
 
 	@Override
 	public void propertyChanged(Property arg0) {
+
+		if (arg0.equals(dataName)) {
+			label.setText(dataName.getValue());
+		}
+
+		if (arg0.equals(leftButtonText)) {
+			leftButton.setText(leftButtonText.getValue());
+		}
+
+		if (arg0.equals(rightButtonText)) {
+			rightButton.setText(rightButtonText.getValue());
+		}
 	}
 
 	@Override
 	public void setValue(Object arg0) {
-		value = (int) arg0;
+		value = (double) arg0;
+
+		if (value != Double.parseDouble(trimValue.getText().trim())) {
+			trimValue.setText(value + "");
+		}
 	}
 
 	@Override
@@ -47,31 +80,39 @@ public class TrimWidget extends Widget implements ActionListener, KeyListener {
 
 		setLayout(new BorderLayout());
 
-		trimLeft = new JButton("<--");
+		leftButton = new JButton("<--");
 		trimValue = new JTextField(value + "");
-		trimRight = new JButton("-->");
+		rightButton = new JButton("-->");
+		label = new JLabel(dataName.getValue());
 
 		trimValue.setEditable(true);
-		trimValue.setHorizontalAlignment(JTextField.CENTER);
+		trimValue.setHorizontalAlignment(SwingConstants.CENTER);
 
-		trimLeft.addActionListener(this);
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+
+		leftButton.addActionListener(this);
 		trimValue.addKeyListener(this);
-		trimRight.addActionListener(this);
+		rightButton.addActionListener(this);
 
-		add(trimLeft, BorderLayout.LINE_START);
+		add(leftButton, BorderLayout.LINE_START);
 		add(trimValue, BorderLayout.CENTER);
-		add(trimRight, BorderLayout.LINE_END);
+		add(rightButton, BorderLayout.LINE_END);
+		add(label, BorderLayout.PAGE_START);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource().equals(trimLeft)) {
-			value--;
+		if (e.getSource().equals(leftButton)) {
+			value = inverted.getValue() ? value
+					+ unitsPerClick.getValue().doubleValue() : value
+					- unitsPerClick.getValue().doubleValue();
 			trimValue.setText(value + "");
 		}
 
-		if (e.getSource().equals(trimRight)) {
-			value++;
+		if (e.getSource().equals(rightButton)) {
+			value = inverted.getValue() ? value
+					- unitsPerClick.getValue().doubleValue() : value
+					+ unitsPerClick.getValue().doubleValue();
 			trimValue.setText(value + "");
 		}
 
@@ -90,14 +131,14 @@ public class TrimWidget extends Widget implements ActionListener, KeyListener {
 	@Override
 	public void keyReleased(KeyEvent e) {
 		if (e.getSource().equals(trimValue)) {
-			
+
 			try {
-				value = Integer.parseInt(trimValue.getText().trim());
+				value = Double.parseDouble(trimValue.getText().trim());
 			} catch (NumberFormatException ex) {
 				return;
 			}
 		}
-		
+
 		NetworkTable.getTable("SmartDashboard").putNumber(dataName.getValue(),
 				value);
 	}
